c1c41eef3fdf1210052dbfd0ebaf83f00d048bf7
brettawaytoday <brettawaytoday@gmail.com>
2021-04-09 14:58:54 +1200

    Initial Commit

---
+++ Water Guardian/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Water Guardian
+//
+//  Created by Brett Christian on 9/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Water Guardian/Water_GuardianApp.swift
@@ -0,0 +1,17 @@
+//
+//  Water_GuardianApp.swift
+//  Water Guardian
+//
+//  Created by Brett Christian on 9/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Water_GuardianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
